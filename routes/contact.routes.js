d1d69e47508d5a766abf0c921d55832d09d60c86
NevishaVora <nevishavora@gmail.com>
2025-02-27 16:13:00 +0530

    add mega news code

--- routes/contact.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createContact , uploadContact, getContacts} = require('../controllers/contact.controller');
-const router = express.Router();
-
-router.post('/add',uploadContact.single('img'), createContact);
-router.get('/get', getContacts);
-module.exports = router;
--- routes/savePost.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { savePost,  getSavedPosts } = require('../controllers/savePost.controller');
-
-const router = express.Router();
-
-router.post("/save/:userId", savePost);
-router.get('/saved/:userId', getSavedPosts); // Get Saved Posts
-
-module.exports = router;
